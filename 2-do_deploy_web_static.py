48343f4fbce724e88b4df5d30f6de74eba54f2c2
olisabelema <olisabelema@gmail.com>
2023-09-12 12:55:38 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,36 +1,15 @@
-#!/usr/bin/python3
-"""
- a Fabric script that generates a .tgz archive
- from the contents of the web_static folder of your
- AirBnB Clone repo, using the function do_pack
-"""
-from fabric.api import *
-from datetime import datetime
-from os.path import exists
-from os import getenv, environ
-
-def do_pack():
-    """ Generates a .tgz archive from the folder web_static folder
-    """
-    local("mkdir -p versions")
-
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_name = "versions/web_static_{}.tgz".format(date)
-
-    result = local("tar -cvzf {} web_static".format(archive_name))
-    if result.succeeded:
-        return archive_name
-    else:
-        return None
-
-"""
-a Fabric script (based on the file 1-pack_web_static.py)
+ Fabric script (based on the file 1-pack_web_static.py)
 that distributes an archive to your web servers, using the function do_deploy
 """
 
+
+from fabric.api import *
+from os.path import exists
+from os import getenv, environ
+
 env.hosts = ['100.25.165.191', '3.83.245.148']
 env.user = 'ubuntu'
-env.key_filename = '/home/root/.ssh/id_rsa'
+env.key_filename = '/home/vagrant/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
@@ -43,29 +22,30 @@ def do_deploy(archive_path):
         archive_name = archive_path.split('/')[-1]
         file_name = archive_name.split('.')[0]
         sym_link = "/data/web_static/current"
-        release_version = f"/data/web_static/releases/{file_name}/"
+        release_version = "/data/web_static/releases/{}/".format(file_name)
 
         # deploying locally
         run_locally = getenv("run_locally", None)
         if run_locally is None:
-            print(f"Deploying new_version from {archive_path}")
-            local(f"sudo mkdir -p {release_version}")
-            local(f"sudo tar -xzf {archive_path} \
+            print("Deploying new_version from {}".format(archive_path))
+            local("sudo mkdir -p {}".format(release_version))
+            local("sudo tar -xzf {}.format(archive_path) \
 -C {release_version} --strip-components=1")
-            local(f"sudo rm -f {sym_link}")
-            local(f"sudo ln -s {release_version} {sym_link}")
+            local("sudo rm -f {}".format(sym_link))
+            local("sudo ln -s {} {}".format(release_version, sym_link))
             environ['run_locally'] = "True"
             print("Deployed locally\n")
 
-        put(archive_path, f"/tmp/{archive_name}")
-        run(f"mkdir -p {release_version}")
-        run(f"tar -xzf /tmp/{archive_name} \
+        put(archive_path, "/tmp/{}".format(archive_name))
+        run("mkdir -p {release_version}".format(release_version))
+        run("tar -xzf /tmp/{archive_name}.format(archive_name) \
 -C {release_version} --strip-components=1")
-        run(f"rm /tmp/{archive_name}")
-        run(f"rm -f {sym_link}")
-        run(f"ln -s {release_version} {sym_link}")
-        print(f"New Version Deployed --> {release_version}")
+        run("rm /tmp/{archive_name}".format(archive_name))
+        run("rm -f {}".format(sym_link))
+        run("ln -s {} {}".format(release_version, sym_link))
+        print("New Version Deployed --> {}".format(release_version))
         return True
     except Exception as e:
-        print(f"Failed to Deploy New Version --> {release_version}\n{str(e)}")
+        print("Failed to Deploy New Version --> {}\n{}".format(release_version, str(e)))
+
         return False
