351a2af057e3ac9ed668d268154252ae392a6f6c
olisabelema <olisabelema@gmail.com>
2023-09-12 12:58:40 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,3 @@
- Fabric script (based on the file 1-pack_web_static.py)
-that distributes an archive to your web servers, using the function do_deploy
-"""
-
-
 from fabric.api import *
 from os.path import exists
 from os import getenv, environ
@@ -15,7 +10,7 @@ env.key_filename = '/home/vagrant/.ssh/id_rsa'
 def do_deploy(archive_path):
     """Deploys the web static to the server"""
     if not exists(archive_path):
-        print("path does not exist\n")
+        print("Path does not exist\n")
         return False
 
     try:
@@ -24,28 +19,25 @@ def do_deploy(archive_path):
         sym_link = "/data/web_static/current"
         release_version = "/data/web_static/releases/{}/".format(file_name)
 
-        # deploying locally
+        # Deploying locally
         run_locally = getenv("run_locally", None)
         if run_locally is None:
             print("Deploying new_version from {}".format(archive_path))
             local("sudo mkdir -p {}".format(release_version))
-            local("sudo tar -xzf {}.format(archive_path) \
--C {release_version} --strip-components=1")
+            local("sudo tar -xzf {} -C {} --strip-components=1".format(archive_path, release_version))
             local("sudo rm -f {}".format(sym_link))
             local("sudo ln -s {} {}".format(release_version, sym_link))
             environ['run_locally'] = "True"
             print("Deployed locally\n")
 
         put(archive_path, "/tmp/{}".format(archive_name))
-        run("mkdir -p {release_version}".format(release_version))
-        run("tar -xzf /tmp/{archive_name}.format(archive_name) \
--C {release_version} --strip-components=1")
-        run("rm /tmp/{archive_name}".format(archive_name))
+        run("mkdir -p {}".format(release_version))
+        run("tar -xzf /tmp/{} -C {} --strip-components=1".format(archive_name, release_version))
+        run("rm /tmp/{}".format(archive_name))
         run("rm -f {}".format(sym_link))
         run("ln -s {} {}".format(release_version, sym_link))
         print("New Version Deployed --> {}".format(release_version))
         return True
     except Exception as e:
         print("Failed to Deploy New Version --> {}\n{}".format(release_version, str(e)))
-
         return False
